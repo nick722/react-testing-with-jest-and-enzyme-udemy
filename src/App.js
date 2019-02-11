2b78f4a9615af20a23ee1eff5d668b7b5ca3c8ea
nick722 <79ZUdtd3orzC!GeTcb^s&79jBB>
2019-02-11 21:58:45 +0300

    15. Test Component Rendering

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import "./App.css";
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div data-test="component-app">
         <h1>Click counter</h1>
       </div>
     );
