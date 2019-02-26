2cd3aede70f146b9bb36c04984aea22c4add6ee5
nick722 <79ZUdtd3orzC!GeTcb^s&79jBB>
2019-02-26 21:02:27 +0300

    28. Code the Congrats Component

--- src/components/Congrats.js
+++ src/components/Congrats.js
@@ -3,8 +3,19 @@ import React from "react";
 /**
  * Funcitonal react component for for congratulatory message.
  * @function
+ * @param {object} props - React props.
  * @returns {JSX.Element} - Rendered component (or null if 'success' prop is false)
  */
-export default () => {
-  return <div />;
+export default props => {
+  if (props.success) {
+    return (
+      <div data-test="component-congrats">
+        <span data-test="congrats-message">
+          Congratulations! You guessed the word!
+        </span>
+      </div>
+    );
+  } else {
+    return <div data-test="component-congrats" />;
+  }
 };
